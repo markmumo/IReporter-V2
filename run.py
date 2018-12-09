06748f960ef36288af7c0d23f739156c69c66ca2
mark <marcmumo@gmail.com>
2018-12-09 20:05:00 +0300

    [Finishes #162412880 ] Add post incident fature to app

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 
 from app import create_app
-from app.api.v1.models import incidents
+from app.api.v2.models import incidents
 
 
 app = create_app(os.getenv("FLASK_CONFIG") or "development")
