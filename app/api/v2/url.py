c7fcf707e654d63195fe1a74565dedb330585e12
mark <marcmumo@gmail.com>
2018-12-12 16:21:41 +0300

    [Feature #162412960] modified get by id incident route

--- app/api/v2/url.py
+++ app/api/v2/url.py
@@ -5,19 +5,19 @@ from app.api.v2.main.views import PostIncidents, AllIncidents, Get_incident_by_i
 
 def url(api):
     api.add_resource(PostIncidents, "/incident")
-    """GET all incidents"""
+    '''GET all incidents'''
     api.add_resource(AllIncidents, "/incident")
 
-    """GET incident by id"""
+    '''GET incident by id'''
     api.add_resource(Get_incident_by_id, "/incident/<int:incident_id>")
 
-    """SignUp"""
+    '''SignUp'''
     api.add_resource(Sign_up, '/auth/Sign_up')
 
-    """SignIn"""
+    '''SignIn'''
     api.add_resource(Sign_in, '/auth/Sign_in')
 
-    """GET all Users"""
+    '''GET all Users'''
     api.add_resource(All_users, '/users')
     api.add_resource(Get_user_by_id, '/users/<int:id>')
     api.add_resource(Get_users_by_email, '/users/<string:email>')
