cc1be0b12507a947833d91b2d7bf2a41d40daba8
mark <marcmumo@gmail.com>
2018-12-13 12:05:50 +0300

    [Chore #162412710] added base test data

--- app/tests/v2/base_test.py
+++ app/tests/v2/base_test.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from migrate import Tables
 import json
 from app import create_app
 
@@ -6,11 +7,15 @@ from app import create_app
 class BaseTest(TestCase):
 
     def setUp(self):
+        ''' test setup'''
+
         app = create_app("testing")
         self.client = app.test_client()
         self.app_context = app.app_context()
-        self.app_context.push()
-        self.app_context.pop()
+        with self.app_context:
+            Tables.drop(self)
+            Tables.create(self)
+            Tables.create_admin(self)
 
         self.create_incident = {
             "created_by": "mark",
@@ -18,114 +23,174 @@ class BaseTest(TestCase):
             "location": "j1.1018° S, 37.0144° juja",
             "status": "draft",
             "image": "corruption.png",
-            "video": "corruption.webm",
+            "video": "corruption.mov",
             "comment": "very serious scandal"
         }
 
-        self.user_signup_data = {
-            "firstname": "markie",
-            "lastname": "fhfjkdhfef",
+        self.edit_data = {
+            "Type": "Intervention",
+            "location": "Roysambu",
+            "image": "betterpic.png",
+            "video": "evenbettervid.mov",
+            "comment": "corruption"
+        }
+
+        self.user_sign_up_data = {
+            "firstname": "mark",
+            "lastname": "mumo",
+            "othernames": "sharkess",
+            "email": "fake@gmail.com",
+            "password": "markie1234",
+            "phoneNumber": '0728124201',
+            "username": "markie",
+            "is_admin": "1"
+        }
+
+        self.user_sign_in_data = {
+            "username": "markie",
+            "password": "markie1234"
+        }
+
+        self.admin_sign_in_data = {
+            "username": "adminirepoter",
+            "password": "admin1234"
+        }
+        self.ghost_user_data = {
+            "username": "ghostbusters",
+            "password": "ghostbusters123"
+        }
+
+        self.invalid_password_data = {
+            "username": "markie",
+            "password": "markie%&\12345"
+        }
+
+        self.existing_user_name_data = {
+            "username": "markie"
+        }
+
+        self.invalid_phone_number_data = {
+            "firstname": "mark",
+            "lastname": "mumo",
+            "othernames": "sharkess",
+            "email": "fake@gmail.com",
+            "password": "fake1234",
+            "phoneNumber": '072800000000000-420-121',
+            "username": "markie",
+            "is_admin": "1"
+        }
+
+        self.invalid_firstname_data = {
+            "firstname": "###@@@###@@",
+            "lastname": "mumo",
+            "othernames": "sharkess",
+            "email": "fake@gmail.com",
+            "password": "fake1234",
+            "phoneNumber": '0728-420-121',
+            "username": "markie",
+            "is_admin": "1"
+        }
+
+        self.invalid_lastname_data = {
+            "firstname": "mark",
+            "lastname": "###@@@###@@",
             "othernames": "sharkess",
             "email": "fakes@gmail.com",
             "password": "fake1234",
-            "phoneNumber": '0728-124-201',
-            "username": "mark",
+            "phoneNumber": '0728124201',
+            "username": "markie",
             "is_admin": "1"
         }
 
-        self.invalid_phone_number = {
-            "firstname": "markie",
-            "lastname": "fhfjkdhfef",
-            "othernames": "shkaesdf",
+        self.invalid_othernames_data = {
+            "firstname": "mark",
+            "lastname": "mumo",
+            "othernames": "###@@@###@@",
             "email": "fakes@gmail.com",
             "password": "fake1234",
-            "phoneNumber": '072800000000000-420-121',
-            "username": "mark",
-            "is_admin": "1"
-        }
-
-        self.invalid_firstname = {
-            "firstname": "#############@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
-            "lastname": "fhfjkdhfef",
-            "othernames": "shaksefs",
-            "email": "fakes@gmail.com",
-            "password": "fake1234",
-            "phoneNumber": '0728-420-121',
-            "username": "mark",
-            "is_admin": "1"
-        }
-
-        self.invalid_lastname = {
-            "firstname": "marekk",
-            "lastname": "#############@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
-            "othernames": "sghesajks",
-            "email": "fakes@gmail.com",
-            "password": "fake1234",
-            "phoneNumber": '0728-420-121',
-            "username": "mark",
-            "is_admin": "1"
-        }
-
-        self.invalid_othernames = {
-            "firstname": "marekk",
-            "lastname": "mummoo",
-            "othernames": "#############@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
-            "email": "fakes@gmail.com",
-            "password": "fake1234",
-            "phoneNumber": '0728-420-121',
-            "username": "mark",
-            "is_admin": "1"
-        }
-
-        self.invalid_password = {
-            "firstname": "marekk",
-            "lastname": "mummoo",
-            "othernames": "markss",
-            "email": "fakessssss@gmail.com",
-            "password": "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!!!!!!!!!!!!!!!!!!!!!!!!!!!",
-            "phoneNumber": '0728-420-121',
-            "username": "mark",
+            "phoneNumber": '0728124201',
+            "username": "markie",
             "is_admin": "1"
         }
 
         self.invalid_username_data = {
-            "username": "@@@@@@@@@@@@",
-            "email": "fakes@gmail.com",
+            "username": "###@@@###@@",
+            "email": "fake@gmail.com",
             "password": "fake1234",
-            "firstname": "markie",
-            "lastname": "fhfjkdhfef",
-            "othernames": "shgakedyds",
-            "phoneNumber": '0728-420-121',
+            "firstname": "mark",
+            "lastname": "mumo",
+            "othernames": "sharkess",
+            "phoneNumber": '0728124201',
             "is_admin": "1"
 
         }
-        self.email_exists = {
+        self.email_exists_data = {
             "username": "markie",
-            "email": "fake@gmail.com",
+            "email": "fakes@gmail.com",
             "password": "fake1234",
-            "firstname": "markie",
-            "lastname": "fhfjkdhfef",
-            "othernames": "shdtjaks",
-            "phoneNumber": '0728-420-121',
+            "firstname": "mark",
+            "lastname": "mumo",
+            "othernames": "sharkess",
+            "phoneNumber": '0728124201',
             "password": "shahkwss123",
             "is_admin": "1"
-
         }
 
     def post_incident(self):
+        ''' create incident function '''
+
+        token = self.get_jwt_token_as_user()
+
         response = self.client.post(
-            "/api/v1/incident",
+            "/api/v2/incident",
             data=json.dumps(self.create_incident),
-            headers={'content-type': 'application/json'}
+            headers={'content-type': 'application/json',
+                     'Authorization': f'Bearer {token}'}
         )
 
         return response
 
-    def signup(self):
-        """ user signup function """
+    def admin_sign_in(self):
+        ''' admin sign_in function '''
+
         response = self.client.post(
-            "api/v1/auth/Sign_up",
-            data=json.dumps(self.user_signup_data),
+            "api/v2/auth/Sign_in",
+            data=json.dumps(self.admin_sign_in_data),
+            headers={
+                'content-type': 'application/json'
+            }
+        )
+        return response
+
+    def sign_up(self):
+        ''' user sign_up function '''
+
+        response = self.client.post(
+            "api/v2/auth/Sign_up",
+            data=json.dumps(self.user_sign_up_data),
             headers={'content-type': 'application/json'}
         )
         return response
+
+    def sign_in(self):
+        ''' user sign_in function '''
+
+        response = self.client.post(
+            "api/v2/auth/Sign_in",
+            data=json.dumps(self.user_sign_in_data),
+            headers={'content-type': 'application/json'}
+        )
+        return response
+
+    def get_jwt_token_as_admin(self):
+        """get jwt token """
+        response = self.admin_sign_in()
+        token = json.loads(response.data).get("token", None)
+        return token
+
+    def get_jwt_token_as_user(self):
+        """get jwt token """
+        self.sign_up()
+        response = self.sign_in()
+        token = json.loads(response.data).get("token", None)
+        return token
