054302a5481f394c06a5eb5014360c0518b89909
mark <marcmumo@gmail.com>
2018-12-16 16:16:14 +0300

    [Bug #162683881] update url.py file

--- app/api/v2/url.py
+++ app/api/v2/url.py
@@ -1,25 +1,23 @@
-from app.api.v2.auth.views import All_users, Get_user_by_id, Get_users_by_email
+from app.api.v2.admin.views import Admin_Get_incident_by_id, Incidents
 from app.api.v2.auth.views import Sign_in, Sign_up
-from app.api.v2.main.views import PostIncidents, AllIncidents, Get_incident_by_id
+from app.api.v2.main.views import AllIncidents, Get_incident_by_id, \
+    PostIncidents
 
 
 def url(api):
     api.add_resource(PostIncidents, "/incident")
     '''GET all incidents'''
     api.add_resource(AllIncidents, "/incident")
+    api.add_resource(Incidents, "/all/incidents")
 
     '''GET incident by id'''
     api.add_resource(Get_incident_by_id, "/incident/<int:incident_id>")
+    api.add_resource(Admin_Get_incident_by_id, "/admin/incident/<int:id>")
 
     '''SignUp'''
     api.add_resource(Sign_up, '/auth/Sign_up')
 
     '''SignIn'''
     api.add_resource(Sign_in, '/auth/Sign_in')
-
-    '''GET all Users'''
-    api.add_resource(All_users, '/users')
-    api.add_resource(Get_user_by_id, '/users/<int:id>')
-    api.add_resource(Get_users_by_email, '/users/<string:email>')
 
     return None
